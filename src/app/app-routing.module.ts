ddbeebd01f052aacdf5b3b16d268fb60656258bb
DiabateJS <diabatejs@yahoo.fr>
2022-01-05 22:20:37 +0100

    Intégration du routage dans l'application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CampagnesComponent} from './campagnes/campagnes.component';
+import {NouvelleCampagneComponent} from './nouvelle-campagne/nouvelle-campagne.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'campagnes',
+    component: CampagnesComponent
+  },
+  {
+    path: 'nouvelle-campagne',
+    component: NouvelleCampagneComponent
+  },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
